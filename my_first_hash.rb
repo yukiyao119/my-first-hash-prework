46bba25a04f1e9cea9d69a7c83610f509c480c95
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-27 19:58:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,17 +1,15 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+
   your_hash = {"my_name" => "Yuki", "location" => "library"}
   
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+
   the_manifest = {
-    "whale bone corsets" => 5
-    "porcelain vases" => 2 
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
     "oil paintings" => 3
   }
 
@@ -24,7 +22,6 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest["oil paintings"]
 end
 
@@ -34,12 +31,10 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+
   shipping_manifest["muskets"] = 2
 
   shipping_manifest["gun powder"] = 4
 
-
-  # return the shipping_manifest hash below
   shipping_manifest
 end
