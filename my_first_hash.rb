553094da22e9e06bc7425a1136b0433ffaec2cec
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-27 19:56:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,8 +37,6 @@ def adding
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2
 
-
-  # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
 
 
